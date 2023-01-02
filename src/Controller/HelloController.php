cda95bc8f7247780f7efc0ffc353466ecc703410
N0mekop <blinkevin@hotmail.com>
2023-01-02 16:19:23 +0100

    Nettoyage HelloController

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -3,7 +3,6 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,16 +13,8 @@ class HelloController extends AbstractController
     public function index(Request $request, string $name = 'Kevin'): Response
     {
 
-        dd($request->headers);
-
-/*         dump($name);
-
         return $this->render('hello/index.html.twig', [
             'controller_name' => 'HelloController',
-        ]); */
-
-        return new JsonResponse([
-            'name' => $name,
         ]);
         
     }
