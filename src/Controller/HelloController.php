0820c4983cf18cf025bd786c2150ce3c3948e0bd
N0mekop <blinkevin@hotmail.com>
2023-01-02 15:08:49 +0100

    Ajout use Request

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -4,14 +4,17 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HelloController extends AbstractController
 {
     #[Route('/hello/{name<\w+>}', methods: [ 'GET' ], name: 'app_hello')]
-    public function index(string $name = 'Kevin'): Response
+    public function index(Request $request, string $name = 'Kevin'): Response
     {
+
+        dd($request->headers);
 
 /*         dump($name);
 
@@ -22,5 +25,6 @@ class HelloController extends AbstractController
         return new JsonResponse([
             'name' => $name,
         ]);
+        
     }
 }
