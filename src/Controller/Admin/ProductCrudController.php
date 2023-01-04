3c47f7ba408dc2bd19a9b7f425378e5706e71326
N0mekop <blinkevin@hotmail.com>
2023-01-04 14:43:18 +0100

    Ajout symfony console form

--- src/Controller/Admin/ProductCrudController.php
+++ src/Controller/Admin/ProductCrudController.php
@@ -3,10 +3,12 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Product;
+use App\Form\ProductImageType;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\CollectionField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\Field;
+use EasyCorp\Bundle\EasyAdminBundle\Field\FormField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\MoneyField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 
@@ -19,10 +21,14 @@ class ProductCrudController extends AbstractCrudController
 
     public function configureFields(string $pageName): iterable
     {
+        yield FormField::addTab('General');
         yield Field::new('name');
         yield AssociationField::new('category')->setRequired(false);
-        yield MoneyField::new('price')->setCurrency('EUR');
-        // yield CollectionField::new('productImages')->hideOnIndex();
+        yield MoneyField::new('price')->setStoredAsCents(false)->setCurrency('EUR');
         yield TextEditorField::new('description')->hideOnIndex();
+
+        yield FormField::addTab('Images');
+        yield CollectionField::new('productImages')->hideOnIndex()
+            ->setEntryType(ProductImageType::class);
     }
 }
