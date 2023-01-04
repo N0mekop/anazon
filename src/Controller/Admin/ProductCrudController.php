421d9ef804af678d2809d7b4250fe4702c2d3b22
N0mekop <blinkevin@hotmail.com>
2023-01-04 11:02:23 +0100

    Création product CRUD

---
+++ src/Controller/Admin/ProductCrudController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Product;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\Field;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+
+class ProductCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Product::class;
+    }
+
+    public function configureFields(string $pageName): iterable
+    {
+        yield Field::new('name');
+        yield Field::new('price');
+        yield TextEditorField::new('description')->hideOnIndex();
+    }
+}
