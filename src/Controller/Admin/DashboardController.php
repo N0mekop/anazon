275b3ba13c97560d1f7291cd2895bbdaf28d3ba6
N0mekop <blinkevin@hotmail.com>
2023-01-05 12:45:15 +0100

    Création UserCrud + modif UserFixtures + UserListener pour utiliser un plainpassword

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -4,6 +4,7 @@ namespace App\Controller\Admin;
 
 use App\Entity\Category;
 use App\Entity\Product;
+use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
@@ -43,6 +44,7 @@ class DashboardController extends AbstractDashboardController
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
+        yield MenuItem::linkToCrud('Utilisateurs', 'fas fa-users', User::class);
         yield MenuItem::linkToCrud('Categories', 'fas fa-list', Category::class);
         yield MenuItem::linkToCrud('Produits', 'fas fa-list', Product::class);
     }
--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -13,7 +13,8 @@ class UserFixtures extends Fixture
         $user = new User();
         $user->setEmail('admin@admin.com');
         $user->setRoles(['ROLE_ADMIN']);
-        $user->setPassword('$2y$13$n9pshnIHabOnMq0Dwz4gGeTOUycyzKE1TraLPtKEwMj2z8YysFhKe');
+        $user->plainPassword = 'demo';
+        // $user->setPassword('$2y$13$n9pshnIHabOnMq0Dwz4gGeTOUycyzKE1TraLPtKEwMj2z8YysFhKe');
         $manager->persist($user);
 
         $manager->flush();
